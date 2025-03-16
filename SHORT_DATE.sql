abb5b26b54d0ea7d1b77a7662556c7392f9b7374
Sajib Sarker <66987905+SajibSarker113@users.noreply.github.com>
2025-03-16 09:35:45 +0600

    Add files via upload

--- SHORT_DATE.sql
+++ SHORT_DATE.sql
@@ -1,24 +1,26 @@
-SET LINES 120
+SET LINES 84
 SET PAGES 5000
 SET NUMFORMAT 999999.99
 
-BREAK ON REPORT ON CAT_CD
+
+
 
 TTITLE CENTER 'DEPOT: '&&SC_CD', Short Date Medicine' SKIP2
 
 SPOOL ON 
 
 SELECT 
-    B.CAT_CD,
-    TO_CHAR(B.CAT_CD || B.PRD_CD) AS CAT_PRD_CD, 
-    SUBSTR(C.PROD_NM, 1, 30) AS NAME, 
-    B.B_NO, 
-    B.EXP_DT, 
-    SUM(B.RV_QTY) AS RV_QTY1, 
+    A.DIV_CD, B.CAT_CD,A.DIV_CD || B.CAT_CD,
+    TO_CHAR(A.DIV_CD || B.CAT_CD || B.PRD_CD) AS DCP_CD, 
+    INITCAP(SUBSTR(C.PROD_NM, 1, 25)) AS MEDICINE_NAME, 
+    NULL AS Blank_column, -- for creating a blank  row
+    B.B_NO,  
     SUM(B.RV_BAL) AS STK_QTY, 
-    SUM(B.RV_BAL / D.MST_PK) AS PETTY, 
-    SUM(B.RV_QTY - B.RV_BAL) AS DIFF,
-    A.ISS_FROM 
+    TO_CHAR(B.EXP_DT,'DD.MM.YY') EXP_DT, 
+    SUM(B.RV_QTY) AS RV_QTY1
+    --SUM(B.RV_BAL / D.MST_PK) AS PETTY, 
+    --SUM(B.RV_QTY - B.RV_BAL) AS DIFF,
+    --A.ISS_FROM 
 FROM 
     INV.FG_RV1 A, 
     INV.FG_RV2 B, 
@@ -35,71 +37,23 @@ WHERE
     AND B.CAT_CD = D.CAT_CD 
     AND B.PRD_CD = D.PRD_CD 
     AND B.TYP_CD = D.TYP_CD 
-    AND A.RV_DT BETWEEN '01-JAN-19' AND '31-DEC-25' 
-    AND B.CAT_CD > '0' 
-    AND A.DIV_CD = '1' 
+    AND A.RV_DT BETWEEN '01-JAN-19' AND '31-DEC-26' 
+    AND B.CAT_CD > '0'
     AND B.TYP_CD = '09' 
-    AND A.RV_TYPE NOT IN ('0')
-    AND B.EXP_DT < '&&EXP_DATE'
+    AND A.RV_TYPE NOT IN ('0') 
+    AND B.EXP_DT <= LAST_DAY(SYSDATE) + 184
 GROUP BY
-    B.CAT_CD, 
-    B.CAT_CD || B.PRD_CD, 
+    A.DIV_CD, B.CAT_CD, 
+    A.DIV_CD || B.CAT_CD || B.PRD_CD, 
     C.PROD_NM, 
-    B.B_NO,
-    B.EXP_DT, 
-    B.RV_QTY, 
-    B.RV_BAL,
-    A.ISS_FROM 
---HAVING (B.RV_QTY - B.RV_BAL) > 0 
-ORDER BY 
-    B.CAT_CD, B.EXP_DT, B.CAT_CD || B.PRD_CD 
-/
-
-SELECT 
-    B.CAT_CD,
-    TO_CHAR(B.CAT_CD || B.PRD_CD) AS CAT_PRD_CD, 
-    SUBSTR(C.PROD_NM, 1, 30) AS NAME, 
     B.B_NO, 
-    B.EXP_DT, 
-    SUM(B.RV_QTY) AS RV_QTY1, 
-    SUM(B.RV_BAL) AS STK_QTY, 
-    SUM(B.RV_BAL / D.MST_PK) AS PETTY, 
-    SUM(B.RV_QTY - B.RV_BAL) AS DIFF,
-    A.ISS_FROM 
-FROM 
-    INV.FG_RV1 A, 
-    INV.FG_RV2 B, 
-    SCENT.INV_PROD C, 
-    SCENT.PROD_RATE D 
-WHERE 
-    A.RV_NO = B.RV_NO 
-    AND A.LOC_CD = '&&SC_CD' 
-    AND (B.RV_BAL) > '0' 
-    AND B.CAT_CD = C.CAT_CD 
-    AND B.PRD_CD = C.PRD_CD 
-    AND B.TYP_CD = C.TYP_CD 
-    AND A.LOC_CD = B.LOC_CD 
-    AND B.CAT_CD = D.CAT_CD 
-    AND B.PRD_CD = D.PRD_CD 
-    AND B.TYP_CD = D.TYP_CD 
-    AND A.RV_DT BETWEEN '01-JAN-19' AND '31-DEC-25' 
-    AND B.CAT_CD > '0' 
-    AND A.DIV_CD = '2' 
-    AND B.TYP_CD = '09' 
-    AND A.RV_TYPE NOT IN ('0')
-    AND B.EXP_DT < '&&EXP_DATE'
-GROUP BY
-    B.CAT_CD, 
-    B.CAT_CD || B.PRD_CD, 
-    C.PROD_NM, 
-    B.B_NO,
-    B.EXP_DT, 
-    B.RV_QTY, 
     B.RV_BAL,
-    A.ISS_FROM 
+    B.EXP_DT, 
+    B.RV_QTY
+    --A.ISS_FROM 
 --HAVING (B.RV_QTY - B.RV_BAL) > 0 
 ORDER BY 
-    B.CAT_CD, B.EXP_DT, B.CAT_CD || B.PRD_CD 
+    A.DIV_CD, B.EXP_DT, A.DIV_CD || B.CAT_CD || B.PRD_CD 
 /
 
 
